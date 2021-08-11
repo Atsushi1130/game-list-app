37c5e9e7e7727aa2cc8e2aab6e0a406cab53a61a
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-08-12 00:48:07 +0900

    create search func

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,39 @@
+class SearchController < ApplicationController
+  require 'json'
+  require 'net/https'
+  require "uri"
+
+  def index
+  end
+
+  def search
+    data = {
+      "q": params[:keyword],
+      "sort": "title",
+      "limit": "1000",
+    }
+    query = data.to_query
+    uri = URI.parse("https://mediaarts-db.bunka.go.jp/api/search?fieldId=game&categoryId=gm-col&subcategoryId=gm306&"+query)
+    http = Net::HTTP.new(uri.host,uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(uri)
+    res = http.request(req)
+    @res_data = JSON.parse(res.body)
+    @keyword = params[:keyword]
+  end
+
+  def detail
+    @title = params[:title]
+    data = {
+      "name": @title,
+    }
+    query = data.to_query
+    uri = URI.parse("https://mediaarts-db.bunka.go.jp/api/search?fieldId=game&categoryId=gm-col&subcategoryId=gm306&"+query)
+    http = Net::HTTP.new(uri.host,uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(uri)
+    res = http.request(req)
+    @res_data = JSON.parse(res.body)
+    @keyword = params[:keyword]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # search controller
+  get '/' => "search#index"
+  get "/search" => "search#search"
+  get "/keyword/:keyword/detail/:title" => "search#detail"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
