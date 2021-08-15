6fd2c747c57a95cc3a169a3175540f6ab59e8566
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-08-15 21:22:47 +0900

    update search func

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,7 +13,7 @@ class SearchController < ApplicationController
       "limit": "1000",
     }
     query = data.to_query
-    uri = URI.parse("https://mediaarts-db.bunka.go.jp/api/search?fieldId=game&categoryId=gm-col&subcategoryId=gm305&sort=date&"+query)
+    uri = URI.parse("https://mediaarts-db.bunka.go.jp/api/search?fieldId=game&categoryId=gm-item&subcategoryId=gm301&sort=date&"+query)
     http = Net::HTTP.new(uri.host,uri.port)
     http.use_ssl = true
     req = Net::HTTP::Get.new(uri)
@@ -28,23 +28,12 @@ class SearchController < ApplicationController
       "name": @title,
     }
     query = data.to_query
-    uri = URI.parse("https://mediaarts-db.bunka.go.jp/api/search?fieldId=game&categoryId=gm-col&subcategoryId=gm305&sort=date&"+query)
+    uri = URI.parse("https://mediaarts-db.bunka.go.jp/api/search?fieldId=game&categoryId=gm-item&subcategoryId=gm301&sort=date&"+query)
     http = Net::HTTP.new(uri.host,uri.port)
     http.use_ssl = true
     req = Net::HTTP::Get.new(uri)
     res = http.request(req)
     @res_data = JSON.parse(res.body)
     @keyword = params[:keyword]
-    @game306Data = nil
-    @colResData = nil
-    if @res_data["record"][0]["metadata"]["ma:variationOf"]
-      @game306Data = @res_data["record"][0]["metadata"]["ma:variationOf"][0]
-      colUri = URI.parse(@game306Data)
-      colHttp = Net::HTTP.new(colUri.host,colUri.port)
-      colHttp.use_ssl = true
-      colReq = Net::HTTP::Get.new(colUri)
-      colRes = colHttp.request(colReq)
-      @colResData = JSON.parse(colRes.body)
-    end
   end
 end
